05e76edbf24a43818f3b623cff00ac411211410f
Ed Escobar <ed@kleva.co>
2026-02-16 04:04:23 +0000

    Wrap app with ThemeProvider in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
+import { ThemeProvider } from "@/components/ThemeProvider";
 import { ToastProvider } from "@/components/Toast";
 import { KeyboardShortcuts } from "@/components/KeyboardShortcuts";
 
@@ -17,15 +18,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
-        <ToastProvider>
-          <KeyboardShortcuts />
-          <div className="flex h-screen bg-gray-50">
-            <Sidebar />
-            <main className="flex-1 overflow-y-auto">
-              {children}
-            </main>
-          </div>
-        </ToastProvider>
+        <ThemeProvider>
+          <ToastProvider>
+            <KeyboardShortcuts />
+            <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
+              <Sidebar />
+              <main className="flex-1 overflow-y-auto">
+                {children}
+              </main>
+            </div>
+          </ToastProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
